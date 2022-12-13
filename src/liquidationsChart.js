34f08ee3441a486e5def7282c0c5989b76718a37
abernatskiy <abernatskiy@toha.li>
2022-12-13 21:53:21 +0900

    Rewriting liquidationsChart as a module
    	modified:   src/liquidationsChart.js

--- src/liquidationsChart.js
+++ src/liquidationsChart.js
@@ -1,32 +1,36 @@
 import { Chart } from 'chart.js/auto'
 
-(async function() {
+export class LiquidationsChart {
+	dataPointsToChartInputs(dataPoints) {
+		let data = [...dataPoints].map(dp => {
+			return { x: dp.timestamp, y: dp.liquidatedCollateralAmount }
+		})
+		return { data }
+	}
 
-	const data = {
-		datasets: [{
-			label: 'liquidations',
-			data: [{x: -10, y: 0}, {x: 0, y: 10}, {x: 10, y: 5}, {x: 0.5, y: 5.5}],
-			backgroundColor: ['rgb(255, 99, 132)', 'rgb(255, 99, 0)', 'rgb(255, 0, 132)', 'rgb(0, 99, 132)'],
-			borderColor: ['rgb(255, 99, 132)', 'rgb(255, 99, 0)', 'rgb(255, 0, 132)', 'rgb(0, 99, 132)'],
-			pointStyle: ['circle', 'cross', 'crossRot', 'rect'],
-			borderWidth: 3,
-			pointRadius: 5
-		}],
-	};
+	constructor(canvas, initialData) {
+		this.data = new Set(initialData)
 
-	const config = {
-		type: 'scatter',
-		data: data,
-		options: {
-			scales: {
-				x: {
-					type: 'linear',
-					position: 'bottom'
+		const chartData = {
+			datasets: [{
+				label: 'AAVE v2 liquidations',
+				borderWidth: 2,
+				pointRadius: 3,
+				...this.dataPointsToChartInputs(this.data)
+			}]
+		}
+
+		const config = {
+			type: 'scatter',
+			data: chartData,
+			options: {
+				scales: {
+					x: { type: 'linear' },
+					y: { type: 'linear' }
 				}
 			}
 		}
-	};
 
-	new Chart(document.getElementById('liquidations-chart'), config);
-
-})();
+		this.chart = new Chart(canvas, config)
+	}
+}
